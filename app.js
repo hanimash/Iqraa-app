47829a856e297f74a8cd2441267559d58a53846f
Hani <hani.mashkawi@gmail.com>
2018-08-29 23:01:48 +0200

    add views- models- controllers- files

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const adminRouter = require('./routes/admin');
+const studentRouter = require('./routes/student');
+const classRouter = require('./routes/class');
+const teacherRouter = require('./routes/teacher');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,7 +23,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/admin', adminRouter);
+app.use('/student', studentRouter);
+app.use('/class', classRouter);
+app.use('/teacher', teacherRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
