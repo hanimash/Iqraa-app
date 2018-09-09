770b32f0304ab80854f91d5c6107e07242fbd4c0
Hani <hanimash@hotmail.com>
2018-09-09 17:51:08 +0200

    setup config schema to save global var in app

---
+++ models/config.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose'),
+      Schema = mongoose.Schema,
+
+const configSchema =new Schema({
+    studentLastNr: Number,
+    TeacherLastNr: Number,
+});
+
+
+module.exports = mongoose.model('Config', configSchema);
